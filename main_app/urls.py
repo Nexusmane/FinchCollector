76840c99fcfc8aaa39a18676e1b47dd9a247a8b9
Jackson Cole <jacksoncole@Jacksons-MacBook-Pro.local>
2022-01-12 17:08:36 -0800

    Add Update & Delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('finchs/', views.finchs_index, name='index'),
     path('finchs/<int:finch_id>/', views.finchs_detail, name='finchs_detail'),
+    path('finchs/create/', views.FinchCreate.as_view(), name='finchs_create'),
+    path('finchs/<int:pk>/update/', views.FinchUpdate.as_view(), name='finchs_update'),
+    path('finchs/<int:pk>/delete/', views.FinchDelete.as_view(), name='finchs_delete'),
     ]
-
-
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -17,4 +18,18 @@ def finchs_index(request):
 def finchs_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finchs/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finchs/'
+
+class FinchUpdate(UpdateView):
+    model = Finch 
+    fields = ['size', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finchs/'
+
 
